a34a3dd4f315aac080d32795a1d3f85a9fa2f2b2
Vittorio Ballestra <vittorio.ballestra@gmail.com>
2023-01-07 18:47:21 +0100

    Adding error when no commmits

--- sv/github.go
+++ sv/github.go
@@ -216,8 +216,10 @@ func (g *GitHubSv) resolveTitleAndDescription(titleOpt optional.String, descript
 	lastIdx := len(logs) - 1
 	if lastIdx > 0 {
 		return titleOpt.Default(logs[lastIdx]), descriptionOpt.Default(strings.Join(logs[0:lastIdx], "\n")), nil
-	} else {
+	} else if lastIdx == 0 {
 		return titleOpt.Default(logs[lastIdx]), "", nil
+	} else {
+		return "", "", fmt.Errorf("no commits from %v to %v, cannot infer pr title", baseBranch, headBranch)
 	}
 
 }
